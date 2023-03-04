5684901e6d96ccff9b6c70233e21c633c311069c
wozDev <lol2302004@gmail.com>
2023-03-04 16:30:10 +0100

    code cleanup

--- src/main/java/wozniaktv/telegramconnector/Main.kt
+++ src/main/java/wozniaktv/telegramconnector/Main.kt
@@ -1,5 +1,6 @@
 package wozniaktv.telegramconnector
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import wozniaktv.telegramconnector.telegram.TelegramManager
 
@@ -8,23 +9,11 @@ class Main : JavaPlugin() {
 
     var tgManager : TelegramManager ? = null
 
-    //var advancedLicense : AdvancedLicense ? = null
-
     var enabledTgBot = false
 
-    //var plEnabled = true
 
     override fun onEnable() {
         setupOrReloadConfig()
-        //advancedLicense = AdvancedLicense(config.getString("licenseKey"), "http://mc-control.thefantasticboy.ovh/verify.php", this)
-        /*if(!checkLicense()){
-            logger.severe("Your license is not valid!")
-            logger.warning("Contact @Lorenzo137 on Telegram to get one!")
-            plEnabled = false
-            disablePlugin()
-            return
-        }*/
-        //licensedConfirmedMessage()
         printAsciiArt()
         setupListeners()
         logger.info("Listeners ready!")
@@ -34,51 +23,6 @@ class Main : JavaPlugin() {
         //startCheckingLicenseTimer()
 
     }
-
-    /*private fun licensedConfirmedMessage(){
-        logger.info("---------------------------------------------------------------")
-        logger.info("")
-        logger.info("Your license is VALID!")
-        logger.info("")
-        logger.info("If you face any issue, or have any question,")
-        logger.info("feel free to contact @Lorenzo137 on Telegram!")
-        logger.info("")
-        logger.info("---------------------------------------------------------------")
-    }
-
-    private fun disablePlugin(){
-        Bukkit.getScheduler().cancelTasks(this)
-        Bukkit.getPluginManager().disablePlugin(this)
-    }*/
-
-    /*private fun checkLicense() : Boolean{
-
-        if(!config.contains("licenseKey")) return false
-        return advancedLicense!!.isValid()==AdvancedLicense.ValidationType.VALID
-
-
-    }
-
-    private fun startCheckingLicenseTimer(){
-
-        object : BukkitRunnable(){
-
-            val licensor = advancedLicense!!
-
-            override fun run() {
-                if(!checkLicense()){
-                    logger.severe("Your license isn't valid.")
-                    logger.severe("Code Error: "+{licensor.isValid().toString()})
-                    if(licensor.isValid()==AdvancedLicense.ValidationType.NOT_VALID_IP){
-                        logger.severe("There is a chance your License Key got Stolen; report this to @Lorenzo137 on TG")
-                    }
-                    disablePlugin()
-                }
-            }
-
-        }.runTaskTimer(this,432000,432000) // Each 6 hours
-
-    }*/
 
     private fun printAsciiArt(){
         logger.info("\n  _______   _                                 _____                            _             \n" +
@@ -99,11 +43,8 @@ class Main : JavaPlugin() {
     }
 
     override fun onDisable() {
-        /*if(!plEnabled){
-            logger.info("License code error: ${advancedLicense!!.isValid()}")
-            return
-        }*/
         enabledTgBot = false
+        Bukkit.getScheduler().cancelTasks(this)
         tgManager!!.sendMessageNotification("Server is <b>offline</b>! =/")
     }
 
